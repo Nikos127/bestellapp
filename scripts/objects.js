586b96b16d8aeca1431af9af712d78d9231353fd
Nikos127 <nikos127@gmail.com>
2026-04-17 08:17:14 +0200

    basket max-height

---
+++ scripts/objects.js
@@ -0,0 +1,110 @@
+let appSettings = {
+    pickup: false
+};
+
+let menuItems = {
+    vorspeisen: {
+        name: 'Vorspeise',
+        image: './assets/icons/olive.png',
+        items: [
+            {
+                'image': 'assets/img/saganaki.webp',
+                'name': 'Saganaki',
+                'Preis': 5.50,
+                'Beschreibung': 'Griechischer Käse, gebraten und mit Zitrone serviert',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/tzatziki.jpg',
+                'name': 'Tzatziki',
+                'Preis': 4.90,
+                'Beschreibung': 'Joghurt mit Gurke, Knoblauch, Olivenöl und Dill',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/dolmadakia.jpg',
+                'name': 'Dolmadakia',
+                'Preis': 5.20,
+                'Beschreibung': 'Gefüllte Weinblätter mit Reis und Kräutern',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/gigantes.jpg',
+                'name': 'Gigantes',
+                'Preis': 5.80,
+                'Beschreibung': 'Weiße Riesenbohnen in Tomatensauce aus dem Ofen',
+                'amount': 0
+            }
+        ]
+    },
+
+    salate: {
+        name: 'Salat',
+        image: './assets/icons/salad.png',
+        items: [
+            {
+                'image': 'assets/img/choriatiki.jpg',
+                'name': 'Choriatiki',
+                'Preis': 7.50,
+                'Beschreibung': 'Griechischer Bauernsalat mit Tomaten, Gurken, Oliven, Zwiebeln und Feta',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/krautsalat.jpg',
+                'name': 'Krautsalat',
+                'Preis': 4.90,
+                'Beschreibung': 'Weißkohlsalat mit Karotten, Essig und Olivenöl',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/tomatensalat.jpg',
+                'name': 'Tomatensalat',
+                'Preis': 5.20,
+                'Beschreibung': 'Frische Tomaten mit Zwiebeln, Oregano und Olivenöl',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/rucola.jpg',
+                'name': 'Rucola Salat',
+                'Preis': 6.20,
+                'Beschreibung': 'Rucola mit Kirschtomaten, Parmesan und Balsamico',
+                'amount': 0
+            }
+        ]
+    },
+
+    hauptgerichte: {
+        name: 'Hauptgericht',
+        image: './assets/icons/main_course.png',
+        items: [
+            {
+                'image': 'assets/img/gyros.jpg',
+                'name': 'Gyros Teller',
+                'Preis': 13.90,
+                'Beschreibung': 'Gyros vom Drehspieß mit Pommes, Tzatziki und Salat',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/souvlaki.jpg',
+                'name': 'Souvlaki',
+                'Preis': 14.50,
+                'Beschreibung': 'Gegrillte Schweinefleischspieße mit Pommes, Tzatziki und Salat',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/bifteki.jpg',
+                'name': 'Bifteki',
+                'Preis': 15.20,
+                'Beschreibung': 'Griechisches Hacksteak gefüllt mit Feta, dazu Pommes und Salat',
+                'amount': 0
+            },
+            {
+                'image': 'assets/img/moussaka.jpg',
+                'name': 'Moussaka',
+                'Preis': 14.90,
+                'Beschreibung': 'Auberginenauflauf mit Hackfleisch, Kartoffeln und Béchamelsauce',
+                'amount': 0
+            }
+        ]
+    }
+};
